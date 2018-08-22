d0a9fb7f259ec43f7b965c9b6de3c5cfb026ba0c
Simon Bos <simonbos9@gmail.com>
2018-08-22 13:38:54 +0200

    SearchView: unused vars and naming improvements

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { Card, CardHeader, Avatar } from '@material-ui/core';
-import red from '@material-ui/core/colors/amber';
-import PersonIcon from '@material-ui/icons/Person';
+import amber from '@material-ui/core/colors/amber';
 
 import profileCardStyle from '../assets/jss/profileCardStyle';
 
@@ -21,7 +20,10 @@ function SearchCard(props) {
               title: classes.cardHeaderTitle,
               subheader: classes.cardHeaderSubTitle
           }}
-          avatar={<Avatar style={{backgroundColor: red[100], color: red[600]}}>{firstName[0]}</Avatar>}
+          avatar={
+              <Avatar style={{backgroundColor: amber[100], color: amber[600]}}>
+                {firstName[0]}
+              </Avatar>}
           title={`${firstName} ${lastName}`}
           subheader={`${corridor}/${room.padStart(4, '0')}`}
         />
