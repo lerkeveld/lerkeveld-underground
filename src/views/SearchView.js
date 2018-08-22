6b1551f28b7bc8f1e558dac10ab2c9ecb855f739
Simon Bos <simonbos9@gmail.com>
2018-08-22 15:00:36 +0200

    SearchView: search functionality

--- src/views/SearchView.js
+++ src/views/SearchView.js
@@ -12,6 +12,29 @@ import data from '../data.js';
 
 class SearchView extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialUsers: 'searches' in data ? data['searches'] : []
+    }
+    this.state.users = this.state.initialUsers;
+  }
+
+  filterUsers(query) {
+    const keywords = query.split(' ').map(keyword => keyword.toLowerCase());
+    const fields = ['firstName', 'lastName'];
+    const users = this.state.initialUsers.filter(user => {
+        return keywords.every(keyword => {
+            return fields.some(field => {
+                return user[field].split(' ').some(part => {
+                    return part.toLowerCase().indexOf(keyword) === 0;
+                });
+            });
+        });
+    });
+    this.setState({users: users});
+  }
+
   render() {
     const { classes } = this.props;
     return (
@@ -25,12 +48,12 @@ class SearchView extends React.Component {
           </Typography>
           <Grid container spacing={16} style={{paddingBottom: '16px'}}>
             <Grid item xs={12} md={4}>
-              <SearchBar />
+              <SearchBar onChange={this.filterUsers.bind(this)} />
             </Grid>
           </Grid>
           <Grid container spacing={16}>
             {
-              data['searches'].map(user => {
+              this.state.users.map(user => {
                 const key = JSON.stringify(user);
                 return <Grid key={key} item xs={12} sm={6} md={4}>
                          <SearchCard
