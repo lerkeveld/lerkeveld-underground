fef328cb6d380b34ea284df4288e89b42bc96abe
Simon Bos <simonbos9@gmail.com>
2018-08-29 17:42:43 +0200

    SearchView: Added Test Data

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default {
+    "user": {
+        "firstName": "Test",
+        "lastName": "Test",
+        "room": "0",
+        "corridor": "N0",
+        "email": "test.test@domain.com",
+        "phone": "0000 00 00 00"
+    },
+    "searches": [
+        {
+            "firstName": "Test",
+            "lastName": "Test",
+            "room": "0",
+            "corridor": "N0",
+            "email": "test.test@domain.com",
+            "phone": "0000 00 00 00"
+        },
+    ]
+}
