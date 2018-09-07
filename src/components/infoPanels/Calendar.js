f20341afd819d3670c7a0d9e00c093548d5cc4a1
Simon Bos <simonbos9@gmail.com>
2018-09-07 19:24:00 +0200

    InfoView: fixed CalendarPanel warning

--- src/components/infoPanels/Calendar.js
+++ src/components/infoPanels/Calendar.js
@@ -17,16 +17,18 @@ function Cantusses(props) {
       <DefaultPanel
         title="Hoe kan ik de Lerkeveld kalender toevoegen?"
         details={
+            <div>
             <Typography>
               De Lerkeveld kalender is een <span className={classes.bold}>publiek toegankelijke Google kalender</span>.
               Via deze <a target="_blank" rel="noopener noreferrer" href="/calendar/">link</a> kan je de kalender <span className={classes.bold}>online bekijken</span>.
               Om de kalender toe te voegen aan <span className={classes.bold}>applicaties</span> zoals Google Calendar en iCloud Calendar, kan je volgende <a target="_blank" rel="noopener noreferrer" href="/calendar/public.ics">link</a> gebruiken.
               Specifieke details om een kalender toe te voegen via URL vind je op volgende pagina's:
-              <ul>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://support.google.com/calendar/answer/37100">Google Calendar</a></li>
-                <li><a target="_blank" rel="noopener noreferrer" href="https://support.apple.com/en-us/HT202361">iCloud Calendar</a></li>
-              </ul>
             </Typography>
+            <ul>
+              <li><Typography><a target="_blank" rel="noopener noreferrer" href="https://support.google.com/calendar/answer/37100">Google Calendar</a></Typography></li>
+              <li><Typography><a target="_blank" rel="noopener noreferrer" href="https://support.apple.com/en-us/HT202361">iCloud Calendar</a></Typography></li>
+            </ul>
+            </div>
         }
       />
   );
