2df5b0f064be75245f051e8b73582569fa6e7a10
Simon Bos <simonbos9@gmail.com>
2018-12-09 13:07:00 +0100

    ProfileView: PrivacyForm form updates

--- src/views/ProfileView/cards/PrivacyFormDialog.js
+++ src/views/ProfileView/cards/PrivacyFormDialog.js
@@ -10,40 +10,61 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 
-function PrivacyFormDialog(props) {
-  const { dialogOpen, handleDialogClose, fullScreen } = props;
+class PrivacyFormDialog extends React.Component {
 
-  return (
-      <Dialog
-        fullScreen={fullScreen}
-        open={dialogOpen}
-        onClose={handleDialogClose}
-        aria-labelledby='passwordform-dialog-title'
-        fullWidth
-      >
-        <DialogTitle id='passwordform-dialog-title'>Update privacy settings</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Als je ervoor kiest om jouw  gegevens te delen, kunnen Lerkies
-            via deze applicatie je gedeelde gegevens zien. Gegevens die
-            worden gedeeld zijn je naam, je kamer, je e-mailadres en je
-            telefoon.
-          </DialogContentText>
-          <FormControlLabel
-            control={<Checkbox />}
-            label="Ik deel mijn gegevens met alle Lerkies."
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleDialogClose} color="primary">
-            Cancel
-          </Button>
-          <Button onClick={handleDialogClose} color="primary">
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
-  );
+  state = {
+    checked: false
+  }
+
+  handleCheckedState = event => {
+    this.setState({checked: event.target.checked});
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // TODO api:user:edit:privacy
+    this.props.handleDialogClose();
+  }
+
+  render() {
+    const { dialogOpen, handleDialogClose, fullScreen, ...rest } = this.props;
+
+    return (
+        <Dialog
+          fullScreen={fullScreen}
+          open={dialogOpen}
+          onClose={handleDialogClose}
+          aria-labelledby='passwordform-dialog-title'
+          fullWidth
+          {...rest}
+        >
+          <DialogTitle id='passwordform-dialog-title'>Update privacy settings</DialogTitle>
+          <DialogContent>
+            <DialogContentText variant="body2">
+              Als je ervoor kiest om jouw  gegevens te delen, kunnen Lerkies
+              via deze applicatie je gedeelde gegevens zien. Gegevens die
+              worden gedeeld zijn je naam, je kamer, je e-mailadres en je
+              telefoon.
+            </DialogContentText>
+            <FormControlLabel
+              control={<Checkbox
+                  checked={this.state.checked}
+                  onChange={this.handleCheckedState}
+              />}
+              label="Ik deel mijn gegevens met alle Lerkies."
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDialogClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleSubmit} color="primary">
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
+    );
+  }
 }
 
 export default withMobileDialog()(PrivacyFormDialog);
