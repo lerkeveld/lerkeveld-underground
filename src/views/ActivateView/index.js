b5940325a41a52a59b728d05832c030a688561b5
Simon Bos <simonbos9@gmail.com>
2019-02-15 11:14:38 +0100

    Ask if user wants to share data when activating

--- src/views/ActivateView/index.js
+++ src/views/ActivateView/index.js
@@ -6,6 +6,9 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+
 import CloseableSnackbar from '../../components/CloseableSnackbar';
 import LoadingButton from '../../components/LoadingButton';
 import PasswordField from '../../components/PasswordField';
@@ -20,6 +23,7 @@ class ActivateForm extends React.Component {
     email: '',
     password: '',
     confirm: '',
+    checked: false,
     errors: {
         email: false,
         password: false,
@@ -76,12 +80,17 @@ class ActivateForm extends React.Component {
     this.setState(stateUpdate);
   }
 
+  handleCheckedState = event => {
+    this.setState({checked: event.target.checked});
+  }
+
   doActivate = () => {
     api.post({
         path: '/auth/activate',
         data: {
             email: this.state.email,
-            password: this.state.password
+            password: this.state.password,
+            isSharing: this.state.checked
         }
     }).then(data => {
         this.setState({submitted: true, submitting: false});
@@ -156,6 +165,13 @@ class ActivateForm extends React.Component {
                      onChange={this.handleConfirmChange}
                      value={this.state.confirm}
                      error={this.state.errors.confirm}
+                   />
+                   <FormControlLabel
+                     control={<Checkbox
+                         onChange={this.handleCheckedState}
+                         checked={this.state.checked}
+                     />}
+                     label="Ik deel mijn contactgegevens met alle Lerkies"
                    />
                    <LoadingButton
                      className={classes.submit}
