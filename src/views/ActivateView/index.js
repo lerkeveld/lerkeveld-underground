4b9cb2921f878a11b9d45d696a948e72192985c1
Simon Bos <simonbos9@gmail.com>
2019-02-15 11:15:49 +0100

    Optimize imports

--- src/views/ActivateView/index.js
+++ src/views/ActivateView/index.js
@@ -3,11 +3,11 @@ import Link from 'react-router-dom/Link'
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 import CloseableSnackbar from '../../components/CloseableSnackbar';
 import LoadingButton from '../../components/LoadingButton';
